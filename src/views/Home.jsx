f0845f40f42ba7d72715e54c325fe3fb77d69221
Lautaro <barrios.g.lautaro@gmail.com>
2024-10-25 16:35:59 -0300

    feat(Home): add h-100dhv

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -2,7 +2,7 @@ import { Lights } from "../components";
 
 const Home = () => {
   return (
-    <section className="flex flex-col items-center justify-center w-full h-screen md:p-0 p-4 bg-[radial-gradient(circle,_#ffffff_50%,_#040d0e_100%)]">
+    <section className="flex flex-col items-center justify-center w-full lg:h-screen h-[100dvh] md:p-0 p-4 bg-[radial-gradient(circle,_#ffffff_50%,_#040d0e_100%)]">
       <Lights />
       <article className="flex flex-col bg-[radial-gradient(circle,_#061516_30%,_#040d0e_100%)] 2xl:p-52 xl:p-40 lg:p-20 p-10 max-w-[100vw] lg:max-w-[50vw] aspect-square rounded-full items-center justify-center space-y-2">
         <h1 className="text-3xl sm:text-4xl text-center text-gray-50 animate-fadeIn cursor-default">
